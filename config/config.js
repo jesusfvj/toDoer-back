a31fd1cce38f67c6467f99069f88dfea8bb957c8
Jesus Velazquez <jesusfvelazquez@hotmail.com>
2023-04-25 01:24:05 +0200

    env example

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ const ENV = process.env.NODE_ENV || "development"
 const CONFIG = {
     development:{
         app:{
-            PORT: process.env.PORT || 4000
+            PORT: process.env.PORT || 4001
         },
         db:{
             uri: process.env.MONGODB_URL
